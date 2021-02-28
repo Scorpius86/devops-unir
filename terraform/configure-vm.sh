2d7d702b81ecdd3b68cc9471c21bec1bdfcccdad
Erick Arostegui Cunza <scorpius86@gmail.com>
2021-02-28 18:06:19 -0500

    update

--- terraform/configure-vm.sh
+++ terraform/configure-vm.sh
@@ -3,59 +3,59 @@ hostname=$2
 type=$3
 user=$4
 pass=$5
-promnt=$hostname'-> '
+prompt=$hostname'-> '
 
-echo $promnt'======================================================'
-echo $promnt'Setting OS...'
-echo $promnt'======================================================'
+echo $prompt'======================================================'
+echo $prompt'Setting OS...'
+echo $prompt'======================================================'
 
-echo $promnt'Update OS'
-sudo dnf update -y
+#echo $prompt'Update OS'
+#sudo dnf update -y
 
-echo $promnt'time synchronization'
+echo $prompt'time synchronization'
 sudo timedatectl set-timezone America/Lima
 sudo dnf install chrony -y
 sudo systemctl enable chronyd
 sudo systemctl start chronyd
 sudo timedatectl set-ntp true
 
-echo $promnt'Disable SELinux'
+echo $prompt'Disable SELinux'
 sudo sed -i s/=enforcing/=disabled/g /etc/selinux/config
 
-echo $promnt'Install nfs dependencies'
+echo $prompt'Install nfs dependencies'
 sudo dnf install nfs-utils nfs4-acl-tools wget -y
 
 sudo yum install dnf-plugins-core -y
 
-echo $promnt'======================================================'
-echo $promnt'Setting Machine' $hostname
-echo $promnt'======================================================'
+echo $prompt'======================================================'
+echo $prompt'Setting Machine' $hostname
+echo $prompt'======================================================'
 echo $hostname
 
-echo $promnt'======================================================'
-echo $promnt'Install Requeriments'
-echo $promnt'======================================================'
+echo $prompt'======================================================'
+echo $prompt'Install Requeriments'
+echo $prompt'======================================================'
 
-echo $promnt'Authorize id_rsa'
+echo $prompt'Authorize id_rsa'
 sudo chmod 400 .ssh/id_rsa
 
-echo $promnt'Install epel-release'
+echo $prompt'Install epel-release'
 echo $pass | sudo -S yum install epel-release -y
 
-echo $promnt'Install python 2'
+echo $prompt'Install python 2'
 sudo dnf install python2 -y
 
-echo $promnt'Install python 3'
+echo $prompt'Install python 3'
 sudo dnf install -y python3 -y
 
-echo $promnt'Install ansible'
+echo $prompt'Install ansible'
 sudo yum install ansible -y
 
-echo $promnt'======================================================'
-echo $promnt'Setting hosts...'
-echo $promnt'======================================================'
+echo $prompt'======================================================'
+echo $prompt'Setting hosts...'
+echo $prompt'======================================================'
 
-echo $promnt'Configure /etc/hosts'
+echo $prompt'Configure /etc/hosts'
 sudo -- sh -c "echo '
 10.0.1.9   configurator-host      configurator-host
 10.0.1.10   k8s-c8-master      k8s-c8-master
@@ -64,29 +64,29 @@ sudo -- sh -c "echo '
 10.0.1.15   k8s-c8-nfs      k8s-c8-nfs
 ' >> /etc/hosts"
 
-echo $promnt'======================================================'
-echo $promnt'Setting SSH...'
-echo $promnt'======================================================'
+echo $prompt'======================================================'
+echo $prompt'Setting SSH...'
+echo $prompt'======================================================'
 
-echo $promnt'Blank /root/.ssh/authorized_keys'
+echo $prompt'Blank /root/.ssh/authorized_keys'
 sudo -- sh -c "cat /dev/null > /root/.ssh/authorized_keys"
 
-echo $promnt'Configure /root/.ssh/authorized_keys'
+echo $prompt'Configure /root/.ssh/authorized_keys'
 sudo -- sh -c "cat .ssh/id_rsa.pub >> /root/.ssh/authorized_keys"
 
-echo $promnt'Configure ~/.ssh/authorized_keys'
+echo $prompt'Configure ~/.ssh/authorized_keys'
 sudo cat /home/$user/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
 
-echo $promnt'Configure /etc/ssh/ssh_config'
+echo $prompt'Configure /etc/ssh/ssh_config'
 sudo -- sh -c "echo '
 Host *
     StrictHostKeyChecking no' >> /etc/ssh/ssh_config"
 
-echo $promnt'======================================================'
-echo $promnt'Setting Ansible...'
-echo $promnt'======================================================'
+echo $prompt'======================================================'
+echo $prompt'Setting Ansible...'
+echo $prompt'======================================================'
 
-echo $promnt'Configure /etc/ansible/hosts'
+echo $prompt'Configure /etc/ansible/hosts'
 sudo -- sh -c "sudo echo '
 [configurator-host]
 configurator-host ansible_user=root
@@ -114,9 +114,9 @@ k8s-c8-node01 ansible_user=root
 k8s-c8-node02 ansible_user=root
 ' >> /etc/ansible/hosts"
 
-echo $promnt'======================================================'
-echo $promnt'Get repo for ansible...'
-echo $promnt'======================================================'
+echo $prompt'======================================================'
+echo $prompt'Get repo for ansible...'
+echo $prompt'======================================================'
 
-echo $promnt'Clone Repo'
-Sudo git clone https://github.com/Scorpius86/devops-unir.git
+echo $prompt'Clone Repo'
+sudo git clone https://github.com/Scorpius86/devops-unir.git
