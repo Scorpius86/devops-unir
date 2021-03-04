3b97328a6c69ad1bcc0385e537b256482ff1a58b
Erick Arostegui Cunza <scorpius86@gmail.com>
2021-03-03 19:51:33 -0500

    update

--- k8s/terraform/security.tf
+++ k8s/terraform/security.tf
@@ -18,6 +18,18 @@ resource "azurerm_network_security_group" "mySecGroup" {
     destination_address_prefix = "*"
   }
 
+  security_rule {
+    name                       = "AllowAllInBound"
+    priority                   = 1010
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
   tags = {
     environment = "CP2"
   }
